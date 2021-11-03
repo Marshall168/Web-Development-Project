05a54d59ad71af821bb9e3dbc599b4a8f4bf5594
Scott <sjones2015@hotmail.co.uk>
2021-11-03 18:57:02 +0000

    added Gender to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->dateTime('date_of_birth')->nullable();
+            $table->string('gender')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
