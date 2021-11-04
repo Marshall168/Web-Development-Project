e3c93cd04a4fa9e41063d078bf3a7449498a91ca
Scott <sjones2015@hotmail.co.uk>
2021-11-04 08:56:14 +0000

    Update AdminTableSeeder.php

--- database/seeders/AdminTableSeeder.php
+++ database/seeders/AdminTableSeeder.php
@@ -13,10 +13,7 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
-        $a = new Admin;
-        $a->name = "Scott Jones";
-        $a->email = "sjones2015@hotmail.co.uk";
-        $a->save();
+        $users = User::factory()->count(10)->create();
 
     }
 }
