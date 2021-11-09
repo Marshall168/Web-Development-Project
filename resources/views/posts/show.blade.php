c3505a9599a26e41f43bdeb0320f1273942f8378
Scott <sjones2015@hotmail.co.uk>
2021-11-09 09:44:20 +0000

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 
     <ul>
-        <li>Subject: {{post->subject}}</li>
-        <li>Body: {{post->body}}</li>
-        <li>Date Posted: {{post->dateposted}}</li>
+        <li>Subject: {{$post->subject}}</li>
+        <li>Body: {{$post->body}}</li>
+        <li>Date Posted: {{$post->dateposted}}</li>
     </ul>   
 
 @endsection
