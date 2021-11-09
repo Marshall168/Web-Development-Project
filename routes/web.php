bbe4f193432975dc33f4719414815afb579e226e
Scott <sjones2015@hotmail.co.uk>
2021-11-09 09:16:58 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/posts', 'PostController@index');
+require __DIR__.'/auth.php';
+
