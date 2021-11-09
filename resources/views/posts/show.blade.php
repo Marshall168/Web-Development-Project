65616a9f0dcf47f70295154871ade00f22954859
Scott <sjones2015@hotmail.co.uk>
2021-11-09 09:42:40 +0000

    Update show.blade.php

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', '')
+
+@section('content')
+
+    <ul>
+        <li>Subject: {{post->subject}}</li>
+        <li>Body: {{post->body}}</li>
+        <li>Date Posted: {{post->dateposted}}</li>
+    </ul>   
+
+@endsection
