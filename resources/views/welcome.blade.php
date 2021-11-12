fab699248235462e33b53341bd81397ce246e96e
Scott <sjones2015@hotmail.co.uk>
2021-11-12 22:59:33 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>The Network</title>
+    <style>
+        .body-bg {
+            background-color: #121212;
+            background-image: linear-gradient(315deg, #111827 0%, #374151 80%);
+        }
+    </style>
+
 </head>
-<body>
-    
+<body class="font-Montserrat">
+    <!-- Header -->
+    <header>
+        <nav class="container">
+            <h1></h1>
+        </nav>
 </body>
 </html>
