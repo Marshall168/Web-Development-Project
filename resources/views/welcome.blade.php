b0407815000e4de487fa9f7e51b17e8b334003a5
Scott <sjones2015@hotmail.co.uk>
2021-12-07 11:55:03 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                     <!-- secondary -->
                 <div class="flex items-center space-x-1">
                     <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Login</a>
-                    <a href="#" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Signup</a>
+                    <a href="sign" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Signup</a>
                 </div>
                     <!-- end secondary -->
 
