61c39252a6de8ccba096128e2a4ae93bee9a40a4
Scott <sjones2015@hotmail.co.uk>
2021-12-07 14:00:31 +0000

    Create homepage.blade.php

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Network- Home</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <style>
+        .body-bg {
+            
+        }
+    </style>
+
+</head>
+<body>
+        <!-- Navbar -->
+    <nav class="bg-gray-100">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between">
+                <div class="flex space-x-4">
+                    <!-- logo -->
+                    <div>
+                        <a href="/" class="flex items-center py-5 px-2 text-gray-700">
+                        <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                        <span class="font-bold">The Network</span>
+                    </div>
+                    <!-- end logo -->
+                <!-- primary -->
+                    <div class= "flex items-center space-x-1">
+                        <a href="news" class="py-5 px-3 text-gray-700 hover:text-gray-900">News</a>
+                        <a href="contact" class="py-5 px-3 text-gray-700 hover:text-gray-900">Contact</a>
+
+                    </div>
+                <!-- end primary -->
+
+                </div>
+                    <!-- secondary -->
+                <div class="flex items-center space-x-1">
+
+                    <a href="login" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Sign Out</a>
+                </div>
+                    <!-- end secondary -->
+
+            </div>  
+        </div>
+    </nav>
+
+    <!--end navbar-->
+    <!--hero-->
+    <section class="hero mb-auto h-screen p-15">
+        <div class="container flex items-center gap-12 mt-14 lg:mt-28">
+            <!--content-->
+            <div class="flex items-center lg:items-start justify-center">
+                <h2 class="text-3xl md:text-4 lg:text-5xl text-gray-700 mb-6 pl-15">
+                    Homepage
+                </h2>
+                <div class ="flex justify-center flex-wrap gap-6">
+                    <button type="button" class=""></button>
+            
+            </div>
+            
+         </div>
+    </section>
+    <!--end hero-->
+    <!-- footer -->
+    <footer class="bg-gray-100 py-8 max-w-7x mx-auto">
+        <div>
+            <a class="justify-center align-items text-gray-700 flex mx-auto">Copyright Reserved</a>
+        </div>
+       
+    </footer>
+</body>
+</html>
