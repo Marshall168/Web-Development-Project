7d4289f7fba6e6b6474aa7f7216e4b1a351db703
Scott <sjones2015@hotmail.co.uk>
2021-12-07 11:23:43 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Network</title>
 
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <style>
+        .body-bg {
+            
+        }
+    </style>
+
 </head>
 <body>
+        <!-- Navbar -->
+    <nav class="bg-gray-100">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between">
+                <div class="flex space-x-4">
+                    <!-- logo -->
+                    <div>
+                        <a href="" class="flex items-center py-5 px-2 text-gray-700">
+                        <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                        <span class="font-bold">The Network</span>
+                    </div>
+                <!-- primary -->
+                    <div class= "flex items-center space-x-1">
+                        <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">News</a>
+                        <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Contact</a>
+
+                    </div>
+
+                </div>
+
+
+                    <!-- secondary -->
+                <div class="flex items-center space-x-1">
+                    <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Login</a>
+                    <a href="#" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Signup</a>
+                </div>
+
+            </div>  
+        </div>
+    </nav>
+ 
         
 </body>
 </html>
