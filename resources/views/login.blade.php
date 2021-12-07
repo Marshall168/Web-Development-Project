e5579a610b5c8364b9418fbdb2cc5d369aebc88c
Scott <sjones2015@hotmail.co.uk>
2021-12-07 14:00:36 +0000

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -37,8 +37,7 @@
                 </div>
                     <!-- secondary -->
                 <div class="flex items-center space-x-1">
-                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Login</a>
-                    <a href="sign" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Signup</a>
+                    <a href="sign" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Sign Out</a>
                 </div>
                     <!-- end secondary -->
 
@@ -62,6 +61,7 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2 ml-3 text-center" for="password">Password</label>
                     <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-t-0 border-l-0 border-r-0 border-gray-300 focus:border-b-purple-600 transition duration-500 px-3 pb-3">
                 </div>
+                <a href="homepage" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300 text-center">Login</a>
                 <div class="">
                     <a href="#" class="text-right">Forgot your password?</a>
                 </div>
