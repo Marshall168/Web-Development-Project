1b6f0ce2e593a03b732ee031296cdc4cdd4a35c9
Scott <sjones2015@hotmail.co.uk>
2021-12-07 12:06:41 +0000

    Create news.blade.php

---
+++ resources/views/news.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Network - News</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <style>
+        .body-bg {
+            
+        }
+    </style>
+
+</head>
+<body>
+        <!-- Navbar -->
+    <nav class="bg-gray-100">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between">
+                <div class="flex space-x-4">
+                    <!-- logo -->
+                    <div>
+                        <a href="/" class="flex items-center py-5 px-2 text-gray-700">
+                        <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                        <span class="font-bold">The Network</span>
+                    </div>
+                    <!-- end logo -->
+                <!-- primary -->
+                    <div class= "flex items-center space-x-1">
+                        <a href="news" class="py-5 px-3 text-gray-700 hover:text-gray-900">News</a>
+                        <a href="contact" class="py-5 px-3 text-gray-700 hover:text-gray-900">Contact</a>
+
+                    </div>
+                <!-- end primary -->
+
+                </div>
+                    <!-- secondary -->
+                <div class="flex items-center space-x-1">
+                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Login</a>
+                    <a href="sign" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Signup</a>
+                </div>
+                    <!-- end secondary -->
+
+            </div>  
+        </div>
+    </nav>
+ 
+        
+</body>
+</html>
