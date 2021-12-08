25ae8c9bbd2349d85535491099c2faca751ed75b
Scott <sjones2015@hotmail.co.uk>
2021-12-08 12:20:09 +0000

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,25 +22,29 @@
                 <div class="flex space-x-4">
                     <!-- logo -->
                     <div>
-                        <a href="/" class="flex items-center py-5 px-2 text-gray-700">
+                        <a href="homepage" class="flex items-center py-5 px-2 text-gray-700">
                         <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                         </svg>
                         <span class="font-bold">The Network</span>
                     </div>
                     <!-- end logo -->
-                <!-- primary -->
+                <!-- primary
                     <div class= "flex items-center space-x-1">
                         <a href="news" class="py-5 px-3 text-gray-700 hover:text-gray-900">News</a>
                         <a href="contact" class="py-5 px-3 text-gray-700 hover:text-gray-900">Contact</a>
 
                     </div>
-                <!-- end primary -->
+                end primary -->
 
                 </div>
                     <!-- secondary -->
                 <div class="flex items-center space-x-1">
 
+                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Settings</a>
+                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Profile</a>
+                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Messages</a>
+                    <a href="login" class="py-5 px-3 text-gray-700 hover:text-gray-900">Notifications</a>
                     <a href="login" class="py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">Sign Out</a>
                 </div>
                     <!-- end secondary -->
