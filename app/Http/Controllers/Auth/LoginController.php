c3f653322ace6004e263fe7d7378cc77f5a0172b
Scott <sjones2015@hotmail.co.uk>
2021-12-08 17:46:32 +0000

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,19 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Httpe\Controller\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  
 
     use AuthenticatesUsers;
 
@@ -37,4 +31,23 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    
+    
+
+
+    public function logout(Request $r)
+    {
+        Auth::guard('web')->logout();
+
+        $r->session()->invalidate();
+
+        $r->session()->regenerateToken();
+
+        return redirect('/');
+
+    }
+
+
 }
