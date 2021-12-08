1d2c7f009938e2c0795e3bf428adfd68aeac9621
Scott <sjones2015@hotmail.co.uk>
2021-12-08 17:55:06 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                 <div class="flex space-x-4">
                     <!-- logo -->
                     <div>
-                        <a href="/" class="flex items-center py-5 px-2 text-gray-700">
+                        <a href="dashboard" class="flex items-center py-5 px-2 text-gray-700">
                         <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                         </svg>
@@ -28,7 +28,7 @@
                 <!-- primary -->
                     <div class= "flex items-center space-x-1">
                     
-                        <a href="contact" class="py-5 px-3 text-gray-700 hover:text-gray-900">Settings</a>
+                        <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Settings</a>
                         <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Profile</a>
                         <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Notifications</a>
 
