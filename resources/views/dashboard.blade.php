fafd437af0aad43e2d460d95e344c9bd3c1b0e68
Scott <sjones2015@hotmail.co.uk>
2021-12-09 12:41:36 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist.js"></script>
 @extends('layouts.app')
     <!-- Navbar -->
     <nav class="bg-gray-100">
@@ -18,7 +19,7 @@
                 <div class="flex space-x-4">
                     <!-- logo -->
                     <div>
-                        <a href="dashboard" class="flex items-center py-5 px-2 text-gray-700">
+                        <a href="users.dashboard" class="flex items-center py-5 px-2 text-gray-700">
                         <svg class="h-6 w-6 mr-2 text-gray-700 font-bold" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                         </svg>
@@ -29,7 +30,6 @@
                     <div class= "flex items-center space-x-1">
                     
                         <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Settings</a>
-                        <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Profile</a>
                         <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Notifications</a>
 
                     </div>
@@ -38,6 +38,7 @@
                 </div>
                     <!-- secondary -->
                 <div class="col-md-8 pt-2">
+                    <a href="profile" class="py-5 px-3 text-gray-700 hover:text-gray-900">Profile</a>
                     <a href="logout" type="submit" class="btn btn-primary py-3 px-3 bg-blue-500 hover:bg-blue-600 rounded text-white transition duration-300">
                         Sign Out
                     </a>
@@ -47,6 +48,7 @@
             </div>  
         </div>
     </nav>
+
 
 @section('content')
     <div class="container">
@@ -61,9 +63,9 @@
                             </div>
                         @endif
 
-                        <h1>Welcome {{$name ?? '' }}<h1>
 
                     </div>
+                    <h1>Welcome back to The Network<h1>
                 </div>
             </div>
         </div>
