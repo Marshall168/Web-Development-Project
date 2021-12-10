8aee3c5efed79fba0b22bad12a86f846fc6f1888
Scott <sjones2015@hotmail.co.uk>
2021-12-10 14:12:34 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 class UserController extends Controller
 {
@@ -29,5 +33,34 @@ class UserController extends Controller
     {
         $users = User::all();
         return view('dashboard', ['users' => $users]);
+    }
+
+    public function getProfile()
+    {
+        return view('users.profile', ['users' => Auth::user()]);
+    }
+
+    public function postSaveProfile(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:30'
+        ]);
+        $user = Auth::user();
+        $user->name = $request['name'];
+        $user->update();
+        $file = $request->file('image');
+        $filename = $request['name'] . '-' . $user->id . '.jpg';
+        if ($file) {
+            Storage::disk('local')->put($filename, File::get($file));
+        }
+
+        return redirect()->route('profile');
+
+
+    }
+    public function getUserImage($filename)
+    {
+        $file = Storage::disk('local')->get($filename);
+        return new Response($file, 200);
     }
 }
