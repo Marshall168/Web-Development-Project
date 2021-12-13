30fb67e56743903cd187116b9476d389f3d33eb2
Scott <sjones2015@hotmail.co.uk>
2021-12-13 17:12:20 +0000

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -29,4 +29,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
+
+
 // });
+
