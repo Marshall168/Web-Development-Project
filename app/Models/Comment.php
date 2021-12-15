855a5bb6677834a84d305cf0bd70ada02688ed70
Scott <sjones2015@hotmail.co.uk>
2021-12-15 15:17:10 +0000

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,11 +9,16 @@ class Comment extends Model
 {
     use HasFactory;
 
-public function post()
-{
-    return $this->belongsTo('App\Models\Post');
-}
-    
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+        
 }
 
 
